1a63481e9523a2765919d5e12950e467a0df15de
richard.rxj@gmail.com <richard.rxj@gmail.com@f6e1e5bc-9036-97d4-4784-1d0e4388f87a>
2014-01-17 07:45:09 +0000

    for DCOSS2014----gateway test added

--- src/dr/alg/anu/au/GC13_Test.java
+++ src/dr/alg/anu/au/GC13_Test.java
@@ -252,7 +252,223 @@ public class GC13_Test {
 	
 	
 	
-	
+	public static void impactGateWayPerformance() throws IOException, SecurityException, IllegalArgumentException, NoSuchMethodException, IllegalAccessException, InvocationTargetException
+	{
+		int[] networkSizeSet={200,300,400,500,600,700,800,900,1000};                     //100,200,300,400,500,600,700,800,900,1000,2000
+		int[] gatewayLimitSet={25, 50, 100, 200};   //50
+		int[] transRangeSet={20,20,20,20,20,20,20,20,20,20,20};   //30    20 for distributed
+		int[] tourTimeSet={100,200,400,800,1600,3200,6400};    //100,200,400,800,1600,3200,6400
+
+	    String[] algSet={"maxUnit","max","randomUnit","random","dis2-3-unitutility","dis2-3-utility","disCombine-unitutility","disCombine-utility"};
+
+	    DecimalFormat df=new DecimalFormat("#.0");	
+
+			String tOutputFileName="test/new/ImpactPerformance/";
+			File tf=new File(tOutputFileName);
+			if(!tf.exists())
+			{
+				tf.mkdirs();
+			}
+			String tFileName="test/new/topology/";
+			tf=new File(tFileName);
+			if(!tf.exists())
+			{
+				tf.mkdirs();
+			}
+			
+			
+			
+			
+			for(int i=0;i<tourTimeSet.length;i++)
+			{
+				int tTourTime=tourTimeSet[i];
+				ExperimentSetting.tourTime=tTourTime;
+				
+				
+			
+				//String outputFileTime=null;
+				//PrintWriter pwTime=null;
+				//outputFileTime=tOutputFileName+"T"+tTourTime+".txt";
+				//pwTime=new PrintWriter(new OutputStreamWriter(new FileOutputStream(outputFileTime)));
+				
+				
+				String[] outputAlgSet=new String[algSet.length];
+			    PrintWriter[] pwAlgSet=new PrintWriter[algSet.length];                        
+				        
+				for(int algI=0;algI<algSet.length;algI++)
+				{
+				 
+					outputAlgSet[algI]=tOutputFileName+algSet[algI]+"-GateWay-T"+tTourTime+".txt";
+					pwAlgSet[algI]=new PrintWriter(new OutputStreamWriter(new FileOutputStream(outputAlgSet[algI])));
+				}
+				
+				
+
+				for(int j=0;j<networkSizeSet.length;j++)
+				{
+					
+					int networkSize=networkSizeSet[j];
+					int transRange=transRangeSet[j];
+					
+					//pwTime.print(networkSize+" ");
+					
+					for(int algI=0;algI<algSet.length;algI++)
+					{
+						ArrayList<LabResult> resultSet=new ArrayList<LabResult>();
+						
+						pwAlgSet[algI].print(networkSize+" ");
+						
+						
+						for(int g=0;g<gatewayLimitSet.length;g++){
+							
+						int gatewayLimit=gatewayLimitSet[g];
+						
+						
+						for(int k=0;k<ExperimentSetting.cishu;k++)
+						{
+												
+							
+							
+							String nFile=tFileName+"node-"+networkSize+"-"+k+".txt";
+							String gFile=tFileName+"gateway-"+networkSize+"-"+k+".txt";
+							BiNetwork bNet=NetworkGenerator.createFromFile(nFile, gFile, gatewayLimit, transRange);
+						
+							
+							
+						
+							
+							ArrayList<GateWay> solution=new ArrayList<GateWay>();
+							LabResult tResult=null;
+								
+							switch(algI)
+							{
+							 case 0:
+								 tResult=GC13_Alg.maxUnitUtilityGainTourDesign(bNet,solution); break;
+							 case 1:
+								 tResult=GC13_Alg.maxUtilityGainTourDesign(bNet,solution); break;
+							 case 2:
+								 tResult=GC13_Alg.randomUnitUtilityGainTourDesign(bNet,solution); break;
+							 case 3:
+								 tResult=GC13_Alg.randomUtilityGainTourDesign(bNet,solution); break;
+							 case 4:
+							 	{
+							 		double iniMinRange=1.75*transRange;
+									double iniMaxRange=2.5*transRange;
+									double deltaRange=0.05*transRange;
+									double iniTimeStamp=30;
+									double deltaTimeStamp=5;
+									String weightMethod="calcMovingTime";
+									String utilityMethod="calcUnitUtilityGain";
+									tResult=GC13_Alg.disTraMaxUtilityGainTourDesign(bNet, iniMinRange, iniMaxRange, deltaRange, iniTimeStamp, deltaTimeStamp, solution,weightMethod,utilityMethod); 
+									break;
+							 	}
+							 case 5:
+							 	{
+							 		double iniMinRange=1.75*transRange;
+									double iniMaxRange=2.5*transRange;
+									double deltaRange=0.05*transRange;
+									double iniTimeStamp=30;
+									double deltaTimeStamp=5;
+									String weightMethod="calcMovingTime";
+									String utilityMethod="calcUtilityGain";
+									tResult=GC13_Alg.disTraMaxUtilityGainTourDesign(bNet, iniMinRange, iniMaxRange, deltaRange, iniTimeStamp, deltaTimeStamp, solution,weightMethod,utilityMethod); 
+									break;
+							 	}
+							 case 6:
+							 	{
+							 		double iniMinRange=1.75*transRange;
+									double iniMaxRange=10*transRange;
+									double deltaRange=0.05*transRange;
+									double iniTimeStamp=0;    // means no timestamp constraint
+									double deltaTimeStamp=5;
+									String weightMethod="calcPriorityWeight";
+									String utilityMethod="calcUnitUtilityGain";
+									tResult=GC13_Alg.disTraMaxUtilityGainTourDesign(bNet, iniMinRange, iniMaxRange, deltaRange, iniTimeStamp, deltaTimeStamp, solution,weightMethod,utilityMethod); 
+
+									break;
+							 	}		
+							 case 7:
+							 	{
+							 		double iniMinRange=1.75*transRange;
+									double iniMaxRange=10*transRange;
+									double deltaRange=0.05*transRange;
+									double iniTimeStamp=0;    // means no timestamp constraint
+									double deltaTimeStamp=5;
+									String weightMethod="calcPriorityWeight";
+									String utilityMethod="calcUtilityGain";
+									tResult=GC13_Alg.disTraMaxUtilityGainTourDesign(bNet, iniMinRange, iniMaxRange, deltaRange, iniTimeStamp, deltaTimeStamp, solution,weightMethod,utilityMethod); 
+									break;
+							 	}							 
+							}
+							//begin of debug
+//							for(int ti=0;ti<solution.size();ti++)
+//							{
+//									System.out.println(solution.get(ti));
+//							}
+							System.out.println("T--"+tTourTime+"!!!!!Completet--"+algSet[algI]+"("+tResult.getTotalUtility()+")--<Round>"+k+"-<Node>"+networkSize);
+							//end of debug
+							resultSet.add(tResult);
+							
+							
+
+						}
+						
+						
+						
+						LabResult gResult=new LabResult();
+						int activeNodes=0;
+						double totalUtility=0;
+						double totalSojournTime=0;
+						double totalMovingTime=0;
+						for(int k=0;k<resultSet.size();k++)
+						{
+							LabResult tResult=resultSet.get(k);
+							activeNodes=activeNodes+tResult.getActiveNodes();
+							totalUtility=totalUtility+tResult.getTotalUtility();
+							totalSojournTime=totalSojournTime+tResult.getTotalSojournTime();
+							totalMovingTime=totalMovingTime+tResult.getTotalMovingTime();				
+						}
+						gResult.setNetworkSize(networkSize);
+						gResult.setTourTime(ExperimentSetting.tourTime);
+						gResult.setActiveNodes(activeNodes/resultSet.size());
+						gResult.setTotalUtility(totalUtility/resultSet.size());
+						gResult.setTotalSojournTime(totalSojournTime/resultSet.size());
+						gResult.setTotalMovingTime(totalMovingTime/resultSet.size());
+						
+						
+						//pwAlgSet[algI].println("utility:"+df.format(gResult.getTotalUtility())+" movingtime:"+df.format(gResult.getTotalMovingTime())+" sojourntime"+df.format(gResult.getTotalSojournTime()));
+						
+						pwAlgSet[algI].print(" "+df.format(gResult.getTotalUtility()));
+						pwAlgSet[algI].flush();
+						
+						
+						//pwTime.print(df.format(gResult.getTotalUtility())+" ");
+					}
+						
+						pwAlgSet[algI].println();
+					}
+					
+					//pwTime.println();
+					//pwTime.flush();
+
+				}
+
+				
+				//pwTime.flush();
+				//pwTime.close();
+				
+				
+				for(int algI=0;algI<algSet.length;algI++)
+				{
+				 
+					pwAlgSet[algI].flush();
+					pwAlgSet[algI].close();
+				}
+				
+
+				
+		}
+	}
 	
 
 			
@@ -264,7 +480,8 @@ public class GC13_Test {
 	public static void main(String[] args) throws IOException, SecurityException, IllegalArgumentException, NoSuchMethodException, IllegalAccessException, InvocationTargetException 
 	{
 			
-		GC13_Test.impactPerformance();
+		//GC13_Test.impactPerformance();
+		GC13_Test.impactGateWayPerformance();
 	
 	}
 
