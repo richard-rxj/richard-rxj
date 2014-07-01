44c329b9f7fc8685bd29a0e7a67d0ec4047f9859
richard.rxj@gmail.com <richard.rxj@gmail.com@f6e1e5bc-9036-97d4-4784-1d0e4388f87a>
2014-07-01 06:14:10 +0000

    initial test completed!!!

--- coverage/src/coverage/test/TestSuite.java
+++ coverage/src/coverage/test/TestSuite.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 public class TestSuite {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		TestCentralized.test();
-		TestFrameworkByTuningWeight.test();
-		TestFrameworkByThreshold.test();
-		TestFrameworkByBudgetFactor.test();
+		
+		//TestCentralized.test();
+		//TestFrameworkByTuningWeight.test();
+		//TestFrameworkByThreshold.test();
+		//TestFrameworkByBudgetFactor.test();
+		PerformanceTest.test();
+		
 	}
 
 }
